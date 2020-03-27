0f2fb81bcb3a957d2256afc4b316844b1f60f393
MiguelNdeCarvalho <geral@miguelndecarvalho.pt>
2020-03-27 15:32:40 +0000

    introduction: 1 Fixed

--- introduction/other/1.js
+++ introduction/other/1.js
@@ -1,13 +1,23 @@
 function argmax (x){
     let temp;
     let temp_pos;
+
+    if(x.length === 0){
+        temp = -Infinity;
+        temp_pos = -1;
+    }
+
     for(let i = 0; i < x.length; i++) {
-        if(x[i] < x[i+1]){
+        if(x[i] > temp || i === 0){
             temp = x[i];
             temp_pos = i;
         }
     }
-    return { index: temp_pos, value: temp };
+    return `{ index: '${temp_pos}', value: ${temp}}`;
 }
 
+console.log(argmax([-1,-3,-42]));
+console.log(argmax([1,2,42]));
+console.log(argmax([2,42,1]));
 console.log(argmax([42,1,2]));
+console.log(argmax([]));
